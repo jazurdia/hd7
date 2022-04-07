c54075ae8d3c5ad66c2c5a652d284d3db63ca032
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2022-04-06 23:07:42 -0600

    update

--- BTree.java
+++ BTree.java
@@ -1,8 +1,50 @@
 public class BTree<T> {
     Nodo raiz;
-    
-    public BTree<T> (){
+
+    public BTree() {
+
+        raiz = null;
 
     }
-    
+
+    public void insertar(int i, T contenido) {
+        Nodo n = new Nodo<T>(i);
+        n.setContenido(contenido);
+
+        if (raiz == null) {
+            raiz = n;
+        } else {
+            Nodo aux = raiz;
+            while (aux != null) {
+                n.setPadre(aux);
+                if (n.getLlave() >= aux.getLlave()) {
+                    aux = aux.derecha; // QUE??
+                } else {
+                    aux = aux.izquierda;
+                }
+            }
+
+            if (n.getLlave() < n.getPadre().getLlave()) {
+                n.padre.izquierda = n;
+            } else {
+                n.padre.derecha = n;
+            }
+
+        }
+
+    }
+
+    public void buscar(Nodo n) {
+        if (n != null) {
+            buscar(n.izquierda);
+            print("" + n.getLlave() + " " + n.getContenido());
+            buscar(n.derecha);
+
+        }
+    }
+
+    public void print(String str) {
+        System.out.println(str);
+    }
+
 }
--- Nodo.java
+++ Nodo.java
@@ -13,4 +13,24 @@ public class Nodo<T> {
         this.contenido = null;
     }
 
+    public void setContenido(T contenido) {
+        this.contenido = contenido;
+    }
+
+    public T getContenido() {
+        return contenido;
+    }
+
+    public void setPadre(Nodo padre) {
+        this.padre = padre;
+    }
+
+    public Nodo getPadre() {
+        return padre;
+    }
+
+    public int getLlave() {
+        return llave;
+    }
+
 }
