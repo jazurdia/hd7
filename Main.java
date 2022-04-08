6de6d0e274159767edc5fa6b11c93d933a594fc0
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2022-04-07 20:25:02 -0600

    update

--- Main.java
+++ Main.java
@@ -1,6 +1,34 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         System.out.println("Hello World!");
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the filepath: ");
+        // String filepath = sc.nextLine();
+
+        // Reader myReader = new Reader(filepath);
+        Reader myReader = new Reader("diccionarioprueba.txt");
+        String[] lines = myReader.readLines();
+
+        String[] spanish_array = new String[lines.length];
+        String[] english_array = new String[lines.length];
+        String[] frech_array = new String[lines.length];
+
+        for (int i = 0; i < lines.length; i++) {
+            String[] temp = lines[i].split(",");
+            english_array[i] = temp[0];
+            spanish_array[i] = temp[1];
+            frech_array[i] = temp[2];
+        }
+
+        // print all the arrays. This is just to check that the file is read correctly
+        for (int i = 0; i < lines.length; i++) {
+            System.out.println(english_array[i] + " " + spanish_array[i] + " " + frech_array[i]);
+        }
 
     }
 }
--- Reader.java
+++ Reader.java
@@ -1,5 +1,7 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Reader {
 
@@ -11,17 +13,20 @@ public class Reader {
 
     // a method to read a file line by line. It returns a string array with the
     // lines of the file
-    public String[] readLines() {
+    public String[] readLines() throws IOException, FileNotFoundException {
         String[] lines = null;
         try {
             FileReader fr = new FileReader(filepath);
             BufferedReader br = new BufferedReader(fr);
             String line;
             int i = 0;
+            int counter = 0;
             while ((line = br.readLine()) != null) {
                 lines[i] = line;
                 i++;
+                counter++;
             }
+
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
